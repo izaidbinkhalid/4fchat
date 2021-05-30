e6723a7e50d633483b4735fa155e137f6337e48f
unknown <ranazaid239@gmail.com>
2021-05-30 20:53:54 +0500

    new build

---
+++ build/precache-manifest.bd793b17e372d762310edd24d99cb39f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db5f00085b0bf2e5853c185dd2ae2f2d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c8c3a0bc621fe910824",
+    "url": "/static/css/2.ad76a385.chunk.css"
+  },
+  {
+    "revision": "3763ecacb3931f93bd47",
+    "url": "/static/css/main.37448acd.chunk.css"
+  },
+  {
+    "revision": "8c8c3a0bc621fe910824",
+    "url": "/static/js/2.1a13ae3c.chunk.js"
+  },
+  {
+    "revision": "2182b49f32ec797f3b9c375cbe5c7fb3",
+    "url": "/static/js/2.1a13ae3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3763ecacb3931f93bd47",
+    "url": "/static/js/main.28eb113d.chunk.js"
+  },
+  {
+    "revision": "45d009624ee73ac83b54",
+    "url": "/static/js/runtime-main.cdd681a7.js"
+  }
+]);
